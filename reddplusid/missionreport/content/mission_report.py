73581d1eb828773fac2d40c3e7a03ed8b9919432
Khairil Yusof <kaeru@wolverine.(none)>
2013-04-01 06:35:04 +0800

    * Indexes all parent fields, so that shared search text for missions,    shows results for misison reports too. Fixes #24
     * Add email distribution list field. re #22

--- reddplusid/missionreport/content/mission_report.py
+++ reddplusid/missionreport/content/mission_report.py
@@ -19,7 +19,9 @@ from z3c.relationfield.schema import RelationList, RelationChoice
 from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.formwidget.autocomplete import AutocompleteFieldWidget
 from plone.formwidget.autocomplete import AutocompleteMultiFieldWidget
-
+from plone.z3cform.textlines.textlines import TextLinesFieldWidget
+from collective.z3cform.widgets.enhancedtextlines import \
+EnhancedTextLinesFieldWidget
 
 
 from reddplusid.missionreport import MessageFactory as _
@@ -61,5 +63,21 @@ class IMissionReport(form.Schema, IImageScaleTraversable):
         description = _(u'In point form, include who should be doing '
         'what.')
     )
-    #FIXME distribution list
+
+    form.widget(mission_distribution=EnhancedTextLinesFieldWidget)
+    mission_distribution = schema.Tuple(
+       title=_(u'Additional Email Distribution List'),
+       description=_(u'Email addresses to which a copy of this '
+           'report should be sent. One email per entry. Click Add  '
+            'after each new entry. eg. '
+           'username@unorcid.org. Authors, members and '
+           'supporting staff are already included and need not '
+           'be specified here.'),
+       default=(),
+       missing_value=(),
+       value_type=schema.TextLine(
+           ),
+       required=False,
+       )
+
 
